2ac0931a4ac8435cef03cfccf576d069c6cdf478
Ryu Satoy <dan_ryuichi@outlook.com>
2024-07-17 17:31:59 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <base-spinner></base-spinner>
+    <!-- <router-view/> -->
+     <button @click="mostrarSpinner()">
+      Mostrar Spinner
+     </button>
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import BaseSpinner from './components/global/BaseSpinner.vue'
+import eventBus from './components/global/eventBus'
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  name: 'App',
+  components: {
+    BaseSpinner
+  },
+  methods: {
+    mostrarSpinner () {
+      console.log('clicado')
+      eventBus.emit('Spinner::show')
     }
   }
 }
+</script>
+
+<style lang="scss">
+
 </style>
---
+++ src/components/global/BaseSpinner.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="base-spinner fa-3x" v-if="visible">
+    <i class="fa fa-circle-notch fa-spin"></i>
+  </div>
+</template>
+
+<script>
+import eventBus from './eventBus.js'
+
+export default {
+
+  data () {
+    return {
+      visible: false
+    }
+  },
+  created () {
+    eventBus.on('Spinner::show', this.setSpinner)
+    eventBus.on('Spinner::hide', this.setSpinner)
+  },
+  methods: {
+    setSpinner () {
+      this.visible = !this.visible
+    }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+  @import '../../assets/scss/variable';
+
+.base-spinner {
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  z-index: 10000000;
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: $dark-medium;
+  color: $featured;
+}
+</style>
